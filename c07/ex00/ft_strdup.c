937f54d01ce938afb167df11199003a40a3e8d5d
ahguney <99873502+ahguney@users.noreply.github.com>
2022-07-28 13:52:16 +0300

    Add files via upload

---
+++ c07/ex00/ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahguney <ahguney@student.42kocaeli.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/14 17:13:18 by ahguney           #+#    #+#             */
+/*   Updated: 2021/12/14 17:13:40 by ahguney          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*copy;
+
+	i = 0;
+	while (src[i])
+		i++;
+	copy = (char *)malloc(sizeof(char) * i + 1);
+	if (copy == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
