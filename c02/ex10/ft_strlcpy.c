a34c1a59253f85d4069648fb33e1c5a7a214adcd
ahguney <99873502+ahguney@users.noreply.github.com>
2022-07-28 13:47:17 +0300

    Add files via upload

---
+++ c02/ex10/ft_strlcpy.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahguney <ahguney@student.42kocaeli.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/01 17:38:31 by ahguney           #+#    #+#             */
+/*   Updated: 2021/12/05 03:27:42 by ahguney          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	x;
+
+	x = ft_strlen(src);
+	i = 0;
+	if (size != 0)
+	{
+		while (src [i] != '\0' && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (x);
+}
