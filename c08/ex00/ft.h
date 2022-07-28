14e937253ce0a0384529e4c2cfc8cd80f1cb8122
ahguney <99873502+ahguney@users.noreply.github.com>
2022-07-28 13:53:06 +0300

    Add files via upload

---
+++ c08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahguney <ahguney@student.42kocaeli.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/16 17:50:10 by ahguney           #+#    #+#             */
+/*   Updated: 2021/12/16 17:56:24 by ahguney          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
