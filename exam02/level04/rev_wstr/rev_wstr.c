a7fd42533ae838a729702469168ab338dc50d32d
ahguney <99873502+ahguney@users.noreply.github.com>
2022-09-29 04:34:56 +0300

    Update rev_wstr.c

--- exam02/level04/rev_wstr/rev_wstr.c
+++ exam02/level04/rev_wstr/rev_wstr.c
@@ -1,29 +1,70 @@
 #include <unistd.h>
+#include <stdlib.h>
 
-void		rev_wstr(char *s, int first)
+void	ft_putstr(char *str)
 {
-	int		j;
-	int		i = 0;
-	if (s[i])
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
 	{
-		while (s[i] && (s[i] == ' ' || s[i] == '\t'))
-			i++;
-		if (!s[i])
-			return ;
-		j = i;
-		while (s[i] && s[i] != ' ' && s[i] != '\t')
-			i++;
-		rev_wstr(&s[i], 0);
-		write(1, &s[j], i - j);
-		if (!first)
-			write(1, " ", 1);
+		write(1, &str[i], 1);
+		i++;
 	}
 }
 
-int			main(int argc, char **argv)
+char	**ft_split(char *str)
 {
-	if (argc == 2)
-		rev_wstr(argv[1], 1);
+	int i;
+	int i2;
+	int i3;
+	char **tab;
+
+	i = 0;
+	i2 = 0;
+	tab = (char**)malloc(sizeof(char) * 100);
+	while (str[i] != '\0')
+	{
+		if (str[i] > 32)
+		{
+			i3 = 0;
+			tab[i2] = (char*)malloc(sizeof(char) * 100);
+			while (str[i] > 32)
+			{
+				tab[i2][i3] = str[i];
+				i++;
+				i3++;
+			}
+			tab[i2][i3] = '\0';
+			i2++;
+		}
+		else
+			i++;
+	}
+	tab[i2] = 0;
+	return (tab);
+}
+
+int		main(int ac, char **av)
+{
+	int i;
+	char **words;
+
+	i = 0;
+	if (ac == 2)
+	{
+		words = ft_split(av[1]);
+		while (words[i] != 0)
+			i++;
+		i--;
+		while (i >= 0)
+		{
+			ft_putstr(words[i]);
+			if (i > 0)
+				write(1, " ", 1);
+			i--;
+		}
+	}
 	write(1, "\n", 1);
 	return (0);
 }
