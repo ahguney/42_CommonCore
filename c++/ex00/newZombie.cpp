d2f8e64b3142cc6d141368536e6f50fcc13e15fb
ahguney <99873502+ahguney@users.noreply.github.com>
2024-02-05 13:56:38 +0300

    Delete c++ directory

--- c++/ex00/newZombie.cpp
+++
@@ -1,8 +0,0 @@
-#include "zombie.class.hpp"
-
-Zombie*	newZombie(std::string name){
-	Zombie	*zombie;
-
-	zombie = new Zombie(name);
-	return (zombie);
-}
--- c++/ex00/randomChump.cpp
+++
@@ -1,7 +0,0 @@
-#include "zombie.class.hpp"
-
-void	randomChump(std::string name){
-	Zombie	zombie(name);
-	
-	zombie.announce();
-}
